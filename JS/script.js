fe96c4900d8ab454882a97b0e5a23fa9348b0ca5
androTS <64521870+androTS@users.noreply.github.com>
2025-11-16 19:23:45 +0100

    shranil

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 
-// Blog posts stored as JavaScript objects
+/* // Blog posts stored as JavaScript objects
 const posts = [
 {
     title: "🚀 Welcome to My Blog",
@@ -20,3 +20,18 @@ el.innerHTML = `
 `;
 blog.appendChild(el);
 });
+
+ */
+
+
+  // Get current year
+  const currentYear = new Date().getFullYear();
+  // Insert it into the span
+  document.getElementById("year").textContent = currentYear;
+
+<script>
+  const now = new Date();
+  const options = { year: 'numeric', month: 'long' };
+  const formattedDate = now.toLocaleDateString('sl-SI', options);
+  document.getElementById("date").textContent = formattedDate;
+</script>
